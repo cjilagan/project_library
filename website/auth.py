4925a74078ad53f3dd28114dff9dbfe1ee9f4ffc
cjilagan <christianjherardilagan@gmail.com>
2025-03-11 22:40:46 +0800

    added new venv, no authentications yet, added routes at auth, no database yet

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,43 @@
-from flask import Blueprint, Flask, render_template, redirect
+from flask import Blueprint, Flask, render_template, redirect, url_for
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
 from .extensions import db
 from .models import User
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/')
+def index():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('index.html', form=form)
+
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@auth.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@auth.route('/homepage')
 def home():
-
-    new_user = User(name = 'anne')
-    db.session.add(new_user)
-    db.session.commit()
-    return 'Hello world'
-
-@auth.route('/login', methods=['POST', 'GET'])
-def login():
-    return render_template('index.html')
+    return render_template('home.html')
