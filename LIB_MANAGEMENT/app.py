a250df8a686cd1134abb89f3647e13eab566d5ad
cjilagan <christianjherardilagan@gmail.com>
2025-03-25 23:24:29 +0800

    added app.app_context() in app.py for debugging purposes, still need to fix to redirect in admin homepage after logging in

--- LIB_MANAGEMENT/app.py
+++ LIB_MANAGEMENT/app.py
@@ -2,5 +2,9 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == ('__main__'):
+with app.app_context():
+    for rule in app.url_map.iter_rules():
+        print(rule)
+
+if __name__ == '__main__':  
     app.run(debug=True)
